8b95c22d901cac45864e12ab388bb1746779d540
Igor Montagner <igordsm@gmail.com>
2019-05-06 13:38:05 -0300

    Arquivos aula 20

---
+++ 20-sinais-I/sinal1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+void sig_handler(int num) {
+    printf("Chamou Ctrl+C\n");
+}
+
+int main() {
+    struct sigaction s;
+    s.sa_handler = sig_handler;
+    sigemptyset(&s.sa_mask);
+    sigaction(SIGINT, &s, NULL);
+
+    printf("Meu pid: %d\n", getpid());
+
+    while(1) {
+        sleep(1);
+    }
+    return 0;
+}
