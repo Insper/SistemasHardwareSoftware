7c51a2b15d5595bfb2a102c8ed52341d2d6a6c32
Igor Montagner <igordsm@gmail.com>
2020-06-11 17:22:11 -0300

    Aula 22: soma global.c

--- 22-threads-II/soma_global.c
+++ 22-threads-II/soma_global.c
@@ -5,6 +5,8 @@
 struct soma_parcial_args {
     double *vetor;
     int start, end;
+
+    pthread_mutex_t *m;
 };
 
 double soma = 0;
@@ -12,7 +14,9 @@ void *soma_parcial(void *_arg) {
     struct soma_parcial_args *spa = _arg;
 
     for (int i = spa->start; i < spa->end; i++) {
+        if (spa->m != NULL) pthread_mutex_lock(spa->m);
         soma += spa->vetor[i];
+        if (spa->m != NULL) pthread_mutex_unlock(spa->m);
     }
 
     return NULL;
@@ -28,14 +32,28 @@ int main(int argc, char *argv[]) {
         scanf("%lf", &vetor[i]);
     }
 
-
-    /* TODO: criar thread_t e soma_parcial_args aqui */
-
+    pthread_t ids[4];
+    struct soma_parcial_args args[4];
+    pthread_mutex_t m;
+    pthread_mutex_init(&m, NULL);
     for (int i = 0; i < 4; i++) {
-        /* TODO: preencher args e lançar thread */
+        args[i].vetor = vetor;
+        args[i].start = i * n / 4;
+        args[i].end = (i + 1) * n / 4;
+        args[i].m = &m;
+        if (i == 3) args[i].end = n;
+
+        pthread_create(&ids[i], NULL, soma_parcial, &args[i]);
+
     }
 
-    /* TODO: esperar pela conclusão*/
+    for (int i = 0; i < 4; i++) {
+        pthread_join(ids[i], NULL);
+
+    }
+
+    pthread_mutex_destroy(&m);
+
 
     printf("Paralela: %lf\n", soma);
 
@@ -44,6 +62,7 @@ int main(int argc, char *argv[]) {
     aa.vetor = vetor;
     aa.start = 0;
     aa.end = n;
+    aa.m = NULL;
     soma_parcial(&aa);
     printf("Sequencial: %lf\n", soma);
 
