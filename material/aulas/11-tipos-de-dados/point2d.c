2cbb4531440af10f31f8136c1c3d0f3bd50e27d8
Igor Montagner <igordsm@gmail.com>
2021-04-08 15:05:35 -0300

    Aula 11 - Tipos abstratos de dados

---
+++ material/aulas/11-tipos-de-dados/point2d.c
@@ -0,0 +1,39 @@
+#include "point2d.h"
+#include <stdlib.h>
+
+struct _p {
+    double x, y;
+};
+
+typedef struct _p Point2D;
+
+
+Point2D *point2D_new(double x, double y) {
+    return NULL;
+}
+
+
+void point2D_destroy(Point2D *p) {
+    
+}
+
+double point2D_get_x(Point2D *p) {
+    return 0;
+}
+
+double point2D_get_y(Point2D *p) {
+    return 0;
+}
+
+Point2D *point2D_add(Point2D *p1, Point2D *p2) {
+    return NULL;
+}
+
+double point2D_theta(Point2D *p1, Point2D *p2) {
+    return 0;
+}
+
+Point2D *point2D_scale(Point2D *p, double s) {
+    return NULL;
+}
+
