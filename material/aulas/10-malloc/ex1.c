2b3cbb27bf619047b8dc6961562d3e5fa12be5fb
Igor Montagner <igordsm@gmail.com>
2021-04-08 16:19:16 -0300

    aula 10 - malloc

---
+++ material/aulas/10-malloc/ex1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 14
+
+int main(int argc, char *argv[]) {
+    int *vetor = malloc(N * sizeof(int));
+    int i;
+
+    for (i = 0; i <= N; i++) {
+        vetor[i] = i;
+    }
+
+    for (i = 0; i <= N; i++) {
+        printf("Elemento %d: %d\n", i+1, vetor[i]);
+    }
+
+
+    return 0;
+}
