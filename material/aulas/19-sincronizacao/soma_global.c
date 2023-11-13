a3f050c1bf82eaf8d8a3d77d27a6dfef88be2980
luba <flubacheski@gmail.com>
2023-11-13 17:23:50 +0000

    soma_global

--- material/aulas/19-sincronizacao/soma_global.c
+++ material/aulas/19-sincronizacao/soma_global.c
@@ -10,10 +10,11 @@ struct soma_parcial_args {
 double soma = 0;
 void *soma_parcial(void *_arg) {
     struct soma_parcial_args *spa = _arg;
-
+    // lock(s);
     for (int i = spa->start; i < spa->end; i++) {
         soma += spa->vetor[i];
     }
+    //unlock(s)
 
     return NULL;
 }
@@ -30,13 +31,26 @@ int main(int argc, char *argv[]) {
 
 
     /* TODO: criar thread_t e soma_parcial_args aqui */
+    pthread_t id_thread[4];
+    struct soma_parcial_args args[4];
     
     for (int i = 0; i < 4; i++) {
         /* TODO: preencher args e lançar thread */
+        args[i].vetor = vetor;
+        args[i].start = i*n/4;
+        if( i == 3 )
+            args[i].end = n;
+        else 
+            args[i].end = (i+1)*n/4;
+
+        pthread_create(&id_thread[i],NULL,soma_parcial,&args[i]);
     }
 
     /* TODO: esperar pela conclusão*/
-
+    for (int i = 0; i < 4; i++) {
+        pthread_join(id_thread[i],NULL);
+    
+    }
     printf("Paralela: %lf\n", soma);
 
     soma = 0;
