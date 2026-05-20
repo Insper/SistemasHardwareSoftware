7f33ce9e0e5e2db50c7f3daad6c7a56f94ad8b11
luba <flubacheski@gmail.com>
2026-05-20 20:39:37 -0300

    aula22

---
+++ content/aulas/22-questoes-de-revisao/parte2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// linha de compilação: 
+
+/* declarar tarefas aqui e lançar no main */
+/* cada bloco na figura deverá ser representado por um print com sua letra */
+
+
+int main() {
+    return 0;
+}
